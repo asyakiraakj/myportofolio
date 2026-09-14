ac806f7e360eb9b843ece08dfd9b032929f16bd0
Syakira <asysyakirakhadijah@gmail.com>
2026-09-14 20:55:25 +0700

    feat: add unit test for skills page

--- main/tests.py
+++ main/tests.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.utils import timezone
 
 from main.models import Experience
+from main.models import Skills
 
 
 class MainTest(TestCase):
@@ -56,3 +57,51 @@ class MainTest(TestCase):
         self.assertFalse(self.experience.is_ongoing)
         self.assertContains(response, "Selesai")
         self.assertNotContains(response, "Sedang berlangsung")
+
+class SkillsPageTest(TestCase):
+    def setUp(self):
+        self.hard_skill = Skills.objects.create(
+            name="Python",
+            type="hard",
+            category="programming",
+            description="Backend web development using Django.",
+            proficiency=9,
+        )
+        # Sample soft skill
+        self.soft_skill = Skills.objects.create(
+            name="Communication",
+            type="soft",
+            category="other",
+            description="I communicate.",
+            proficiency=8,
+        )
+
+    def test_skills_url_and_template(self):
+        response = self.client.get(reverse("main:show_skills"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "skills.html")
+
+        self.assertContains(response, "<title>Skills - Syakira</title>")
+        self.assertContains(response, f'href="{reverse("main:show_main")}"')
+        self.assertContains(response, f'href="{reverse("main:show_experience")}"')
+        self.assertContains(response, f'href="{reverse("main:show_skills")}"')
+
+    def test_skills_page(self):
+        response = self.client.get(reverse("main:show_skills"))
+
+        self.assertContains(response, self.hard_skill.name)
+        self.assertContains(response, self.hard_skill.description)
+        self.assertContains(response, "Proficiency: 9/10")
+        self.assertContains(response, self.hard_skill.category)
+
+        self.assertContains(response, self.soft_skill.name)
+        self.assertContains(response, self.soft_skill.description)
+        self.assertContains(response, "Proficiency: 8/10")
+
+    def test_skills_page_empty_state(self):
+        Skills.objects.all().delete()
+        response = self.client.get(reverse("main:show_skills"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Belum ada skill yang ditambahkan", count=2) #count=2: "Belum ada skill yang ditambahkan" ada 2 (soft & hard)
