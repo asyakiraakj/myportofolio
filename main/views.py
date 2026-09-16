ed3014b5b71012603b604fc232406ca9c49a5b1c
Syakira <asysyakirakhadijah@gmail.com>
2026-09-16 17:18:31 +0700

    feat(tutorial): implementasi form & data delivery

--- main/views.py
+++ main/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
-
 # Create your views here.
 from django.shortcuts import render
 
 from main.models import Experience
 from main.models import Skills
+from main.forms import SkillsForm
 
+from django.contrib import messages
+from django.core import serializers
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
 
 def show_main(request):
     context = {
@@ -38,3 +41,17 @@ def show_skills(request):
     }
 
     return render(request, "skills.html", context)
+
+def create_skill(request):
+    form = SkillsForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        messages.success(request, "Skill baru berhasil ditambahkan!")
+        return redirect("main:show_skills")
+
+    context = {
+        "name": "Syakira",
+        "form": form,
+    }
+    return render(request, "skills_form.html", context)
--- portofolio/settings.py
+++ portofolio/settings.py
@@ -153,3 +153,6 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+# gunakan https:// untuk trailing urlnya
+CSRF_TRUSTED_ORIGINS = ["https://khadijah-asy-myportofolio.pws.cs.ui.ac.id"]
