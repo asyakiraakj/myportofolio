088361104fc133fa95957a994c1151c40b72ca11
Syakira <asysyakirakhadijah@gmail.com>
2026-09-16 20:26:55 +0700

    feat(tutorial): menyesuaikan dengan tugas2, implementasi data delivery dengan JSON

--- main/views.py
+++ main/views.py
@@ -31,15 +31,25 @@ def show_experience(request):
     return render(request, "experience.html", context)
 
 def show_skills(request):
-    hard_skills = Skills.objects.filter(type='hard')
-    soft_skills = Skills.objects.filter(type='soft')
+    json_response = get_skills_json(request)
+
+    skills = serializers.deserialize(
+        "json",
+        json_response.content.decode("utf-8"),
+    )
+    skills = [skill.object for skill in skills]
+    name_query = request.GET.get("name", "").strip()
+
+    hard_skills = [skill for skill in skills if skill.type == 'hard']
+    soft_skills = [skill for skill in skills if skill.type == 'soft']
 
     context = {
         "name": "Syakira",
+        "skills_list": skills,
         "hard_skills": hard_skills,
-        "soft_skills": soft_skills
+        "soft_skills": soft_skills,
+        "name_query": name_query,
     }
-
     return render(request, "skills.html", context)
 
 def create_skill(request):
@@ -55,3 +65,23 @@ def create_skill(request):
         "form": form,
     }
     return render(request, "skills_form.html", context)
+
+def delete_skill(request, skill_id):
+    skill = get_object_or_404(Skills, pk=skill_id)
+
+    if request.method == "POST":
+        skill.delete()
+        messages.success(request, "Skill berhasil dihapus!")
+        return redirect("main:show_skills")
+
+    return redirect("main:show_skills")
+
+def get_skills_json(request):
+    name_query = request.GET.get("name", "").strip()
+    skills = Skills.objects.all()
+
+    if name_query:
+        skills = skills.filter(name__icontains=name_query)
+
+    skills_json = serializers.serialize("json", skills)
+    return HttpResponse(skills_json, content_type="application/json")
